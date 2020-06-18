3e4b5714d58ff374481e074e2113c92853ba4999
Thor40 <nicholas.kosik@gmail.com>
2020-06-18 18:27:44 -0500

    set up npm packages

---
+++ models/User.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes, STRING } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// create our User model
+class User extends Model {}
+
+User.init(
+  {
+     username: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true
+        }
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [4]
+        }
+      }
+    },
+    {
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'user'
+    }
+  );
+
+module.exports = User;
