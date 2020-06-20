f0538f3b4036cbf044176a97b1f0b93ffd066828
Thor40 <nicholas.kosik@gmail.com>
2020-06-19 19:00:04 -0500

    post routes

--- models/Index.js
+++ models/Index.js
@@ -3,4 +3,16 @@ const Post = require('./Post');
 const Like = require('./Like');
 const Comment = require('./Comment');
 
-module.exports = { User, Post, Like, Comment };
+// associations between models
+
+// Post belongs to User
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+// post belongs to one user, but not many users
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+module.exports = { User, Post };
